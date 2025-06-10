b5c0a8a3e71648f5d9d6d49d7d17bedef681d2c5
Sasank-V <sasanklearner@gmail.com>
2025-06-10 18:06:02 +0530

    Initial Commit

---
+++ client/app/(tabs)/index.tsx
@@ -0,0 +1,24 @@
+import { Stack } from 'expo-router';
+
+import { StyleSheet, Text, View } from 'react-native';
+
+import { ScreenContent } from '~/components/ScreenContent';
+
+export default function Home() {
+  return (
+    <>
+      <Stack.Screen options={{ title: 'Tab One' }} />
+      <View style={styles.container}>
+        <ScreenContent path="app/(tabs)/index.tsx" title="Tab One" />
+        <Text className="bg-blue-600 text-white">Hello World</Text>
+      </View>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+  },
+});
---
+++ server/src/index.ts
@@ -0,0 +1,17 @@
+// src/index.ts
+import express, { Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hotel Management Backend (TypeScript) is running!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
