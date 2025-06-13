dc4d260023b816bf1aa05cb82943a8d360ddc7a8
Sasank-V <sasanklearner@gmail.com>
2025-06-13 11:18:08 +0530

    fix(prisma): add prisma client types

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,14 +1,12 @@
 // src/index.ts
 import express, { Request, Response } from "express";
-import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient();
+import { prisma } from "./lib/prisma";
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.get("/", (req: Request, res: Response) => {
+app.get("/", async (req: Request, res: Response) => {
   res.send(
     "Hotel Management Backend (TypeScript) is running! - Server Branch Created"
   );
